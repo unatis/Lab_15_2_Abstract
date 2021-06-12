eb48e580447319cebf37aa0d45f318fcf56875a0
Yuri Goncharov <ygpanzer@gmail.com>
2021-06-12 12:41:37 +0200

    Create Animal.java

---
+++ Animal.java
@@ -0,0 +1,13 @@
+package com.company;
+
+abstract class Animal {
+
+    // Abstract method (does not have a body)
+    public abstract void animalSound();
+
+    // Regular method
+    public void sleep() {
+        System.out.println("Zzz");
+    }
+
+}
