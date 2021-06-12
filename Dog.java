ed67fc73db4e13af30db7997ddc28014902b7757
Yuri Goncharov <ygpanzer@gmail.com>
2021-06-12 12:41:56 +0200

    Create Dog.java

---
+++ Dog.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Dog extends Animal{
+
+    public void animalSound() {
+
+        // The body of animalSound() is provided here
+        System.out.println("The dog says: woof woof");
+    }
+}
